c6359a41131ef8fe4b77afa4f74f6169a2602ea0
marty191189 <108819400+marty191189@users.noreply.github.com>
2022-07-07 13:19:18 +0300

    Delete multiply_projects_creation.py

--- multiply_projects_creation.py
+++
@@ -1,6 +0,0 @@
-name_architect = input()
-number_of_projects = int(input())
-
-hours = number_of_projects * 3
-
-print(f"The architect {name_architect} will need {hours} hours to complete {number_of_projects} project/s.")
