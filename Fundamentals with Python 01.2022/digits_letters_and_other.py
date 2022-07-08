a8eb8899316fa62b115f8f56506564bf1c59720f
marty191189 <108819400+marty191189@users.noreply.github.com>
2022-07-08 16:42:44 +0300

    Add files via upload

---
+++ Fundamentals with Python 01.2022/digits_letters_and_other.py
@@ -0,0 +1,20 @@
+word = input()
+
+digits = ""
+letters = ""
+others = ""
+
+for char in word:
+
+    if char.isdigit():
+        digits += char
+
+    elif char.isalpha():
+        letters += char
+
+    else:
+        others += char
+
+print(digits)
+print(letters)
+print(others)
