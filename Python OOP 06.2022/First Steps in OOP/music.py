17d5e88f2cb9345e4a525574b59b455587e70b66
marty191189 <108819400+marty191189@users.noreply.github.com>
2022-07-08 18:58:12 +0300

    Add files via upload

---
+++ Python OOP 06.2022/First Steps in OOP/music.py
@@ -0,0 +1,18 @@
+class Music:
+
+    def __init__(self, title, artist, lyrics):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self):
+        result = f'This is "{self.title}" from "{self.artist}"'
+        return result
+
+    def play(self):
+        return self.lyrics
+
+
+song = Music("Title", "Artist", "Lyrics")
+print(song.print_info())
+print(song.play())
