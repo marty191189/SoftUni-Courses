337dd83190b0f381872d40abf2463b1ab4d03263
marty191189 <108819400+marty191189@users.noreply.github.com>
2022-07-08 16:35:36 +0300

    Add files via upload

---
+++ Fundamentals with Python 01.2022/courses.py
@@ -0,0 +1,10 @@
+number = int(input())
+
+list_of_courses = []
+
+for i in range(1, number + 1):
+    current_course = input()
+
+    list_of_courses.append(current_course)
+
+print(list_of_courses)
