24c3f548593855abf04c3d3ff5af84883126c5f7
marty191189 <108819400+marty191189@users.noreply.github.com>
2022-12-09 23:55:52 +0200

    Add files via upload

--- Python OOP 06.2022/Workshop/custom_list/custom_list.py
+++ Python OOP 06.2022/Workshop/custom_list/custom_list.py
@@ -1,12 +1,21 @@
 from copy import deepcopy
 
-class CustomList:
+class NoElementsInTheListError(Exception):
+    pass
+
+class NoSuchValueError(Exception):
+    pass
+
+class CustomIntList:
 
     def __init__(self):
         self.__values = []
 
-    def append(self, val):
-        self.__values.append(val)
+    def append(self, values):
+        if not isinstance(values, int):
+            raise ValueError("Only integers are accepted!")
+
+        self.__values.append(values)
 
     def remove(self, index):
 
@@ -30,9 +39,65 @@ class CustomList:
 
     def extend(self, values):
 
+        for el in values:
+            if not isinstance(el, int):
+                raise ValueError("Only integers are accepted!")
+
         try:
             self.__values.extend(values)
             return deepcopy(self.__values)
 
         except TypeError:
             raise ValueError("Extend method only works with iterable objects.")
+
+    def insert(self, index, value):
+
+        try:
+            if index < 0 or len(self.__values) <= index:
+                raise IndexError
+
+            self.__values.insert(index, value)
+
+            return self.__values
+
+        except IndexError:
+            raise IndexError("Invalid index.")
+
+        except TypeError:
+            raise ValueError("Index is not a valid integer. Please pass a integer number.")
+
+    def pop(self):
+
+        if not self.__values:
+            raise NoElementsInTheListError("No elements in the list.")
+
+        return self.__values.pop()
+
+    def clear(self):
+        self.__values.clear()
+
+    def index_left(self, value):
+
+        try:
+            return self.__values.index(value)
+
+        except ValueError:
+            raise NoSuchValueError("No such value in the list.")
+
+    def index_right(self, value):
+
+        for index in range(len(self.__values) - 1, -1, -1):
+
+            if self.__values[index] == value:
+                return index
+
+        raise NoSuchValueError("No such value in the list.")
+
+    def count(self, value):
+        return self.__values.count(value)
+
+    def reverse(self):
+        return self.__values[::-1]
+
+    def copy(self):
+        return deepcopy(self.__values)
