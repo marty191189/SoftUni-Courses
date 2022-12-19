0401becd86d13a99a4289ad99fe9e80d14b66bb4
marty191189 <108819400+marty191189@users.noreply.github.com>
2022-12-19 23:51:19 +0200

    Update test_custom_hash_table.py

--- Python OOP 06.2022/Workshop/Custom Hash Table/test_custom_hash_table.py
+++ Python OOP 06.2022/Workshop/Custom Hash Table/test_custom_hash_table.py
@@ -1,9 +1,13 @@
 from unittest import main, TestCase
-
+from custom_hash_table import HashTable
 
 class TestHashTable(TestCase):
-    pass
 
+    def test_init(self):
+        table = HashTable()
+        self.assertEqual([None, None, None, None], table._HashTable__keys)
+        self.assertEqual([None, None, None, None], table._HashTable__values)
+        self.assertEqual(4, table.max_capacity)
 
 if __name__ == "__main__":
     main()
