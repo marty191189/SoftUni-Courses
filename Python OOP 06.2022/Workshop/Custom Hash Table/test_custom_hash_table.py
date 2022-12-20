eae6dea080aa7b0fe5e5f3484ecc14b09da7fedb
marty191189 <108819400+marty191189@users.noreply.github.com>
2022-12-20 23:19:26 +0200

    Update test_custom_hash_table.py

--- Python OOP 06.2022/Workshop/Custom Hash Table/test_custom_hash_table.py
+++ Python OOP 06.2022/Workshop/Custom Hash Table/test_custom_hash_table.py
@@ -9,5 +9,27 @@ class TestHashTable(TestCase):
         self.assertEqual([None, None, None, None], table._HashTable__values)
         self.assertEqual(4, table.max_capacity)
 
+    def test_get_item_dunder(self):
+        table = HashTable()
+        table["name"] = "Test"
+        table["age"] = 0
+
+        result = table["name"]
+
+        self.assertEqual("Test", result)
+
+    def test_get_item_dunder_non_existing_key_raises(self):
+        table = HashTable()
+        table["name"] = "Test"
+        table["age"] = 0
+
+        with self.assertRaises(KeyError) as error:
+            # Non-existing key
+            result = table["asd"]
+        self.assertEqual("asd", str(error.exception.args[0]))
+
+    def test_set_item_dunder_(self):
+        pass
+
 if __name__ == "__main__":
     main()
